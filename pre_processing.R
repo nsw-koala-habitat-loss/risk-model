d5c0ac53e26d3ea9e821140972d06b07cb92b90e
Yi Fei Chung <uqychun7@uq.edu.au>
2024-06-10 23:09:00 +1000

    Updated for new covariates processing

--- pre_processing.R
+++ pre_processing.R
@@ -8,6 +8,7 @@ library(exactextractr)
 library(terra)
 library(tidyverse)
 library(spdep)
+library(readxl)
 
 # load functions
 source("functions.R")
@@ -32,79 +33,53 @@ Stack <- terra::rast(list(aloss = Ag_Loss, iloss = In_Loss, floss = Fo_Loss, woo
 # save raster stack
 saveRDS(Stack, file = "output/raster_stacks/woodyextloss.rds")
 
-# proposed covariates based on workshops
-
-#Agriculture:
-#Land use
-#Combined Drought Indicator
-#Property size
-#Property value
-#Distance to nearest SUA (significant urban area)
-#Ecological condition
-#Precipitation
-#Temperature
-#Slope
-#Soil fertility
-
-#Infrastructure:
-#Land use
-#Property size
-#Property value
-#Distance to nearest SUA
-#Population density
-#Ecological condition
-#Slope
-
-#Forestry:
-#Land use
-#Combined Drought Indicator
-#Forest tenure
-#Property size
-#Property value
-#Household income
-#Ecological condition
-#Fire
-#Slope
+# Load proposed covariates based on workshops from lookup xlsx
+CovLookup <- readxl::read_xlsx("Input/covariates/covariate_description.xlsx", sheet = "AllLyr")
 
 # load covariates
-# PROBABLY NEEDS TO CHANGE - FEI TO LOOK AT
+Cov_file_bn <- tools::file_path_sans_ext(basename(list.files("input/covariates", pattern = "\\.tif$", full.names = TRUE)))
+for(i in 1:nrow(CovLookup)){
+  if (CovLookup$`Covariate Code`[i] %in% Cov_file_bn){
+    print(paste0("Load ... ", CovLookup$Type[i] ," ... ", CovLookup$`Covariate Code`[i], " = ",  CovLookup$`Covariate Description`[i]))
+    assign(CovLookup$`Covariate Code`[i], terra::rast(paste0("input/covariates/", CovLookup$`Covariate Code`[i], ".tif")))
+  }
+}    
 
-# combined drought indicator
-Drought <- terra::rast("input/covariates/drought.tif")
-# elevation
-Elev <- terra::rast("input/covariates/elev.tif")
-ELEV_NA <- not.na(Elev)
-# fire history (2011-2019)
-Fire <- terra::rast("input/covariates/fire.tif")
-# forest code
-ForCode <- terra::rast("input/covariates/forest_code.tif")
-# forest tenure
-ForTen <- terra::rast("input/covariates/forest_tenure.tif")
-# forest tenure type
-ForTenType <- terra::rast("input/covariates/forest_tenure_type.tif")
-# 2016 median household income weekly
-Income <- terra::rast("input/covariates/income.tif")
-# land use (NSW landuse 2013)
-LandUse <- terra::rast("input/covariates/landuse.tif")
-# population density
-PopDen <- terra::rast("input/covariates/pop_den.tif")
-# precipitation
-Precip <- terra::rast("input/covariates/prec.tif")
-# remoteness structure (ABS)
-Remote <- terra::rast("input/covariates/remoteness.tif")
-# slope
-Slope <- terra::rast("input/covariates/slope.tif")
-# soil fertility
-SoilFert <- terra::rast("input/covariates/soil_fert.tif")
-# soil nitrogen
-SoilNit <- terra::rast("input/covariates/soil_nitrogen.tif")
-# soil type
-SoilType <- terra::rast("input/covariates/soil_type.tif")
-# temperature
-Temp <- terra::rast("input/covariates/temp.tif")
-# # Property size
+# # combined drought indicator
+# Drought <- terra::rast("input/covariates/drought.tif")
+# # elevation
+# Elev <- terra::rast("input/covariates/elev.tif")
+# # fire history (2011-2019)
+# Fire <- terra::rast("input/covariates/fire.tif")
+# # forest code
+# ForCode <- terra::rast("input/covariates/forest_code.tif")
+# # forest tenure
+# ForTen <- terra::rast("input/covariates/forest_tenure.tif")
+# # forest tenure type
+# ForTenType <- terra::rast("input/covariates/forest_tenure_type.tif")
+# # 2016 median household income weekly
+# Income <- terra::rast("input/covariates/income.tif")
+# # land use (NSW landuse 2013)
+# LandUse <- terra::rast("input/covariates/landuse.tif")
+# # population density
+# PopDen <- terra::rast("input/covariates/pop_den.tif")
+# # precipitation
+# Precip <- terra::rast("input/covariates/prec.tif")
+# # remoteness structure (ABS)
+# Remote <- terra::rast("input/covariates/remoteness.tif")
+# # slope
+# Slope <- terra::rast("input/covariates/slope.tif")
+# # soil fertility
+# SoilFert <- terra::rast("input/covariates/soil_fert.tif")
+# # soil nitrogen
+# SoilNit <- terra::rast("input/covariates/soil_nitrogen.tif")
+# # soil type
+# SoilType <- terra::rast("input/covariates/soil_type.tif")
+# # temperature
+# Temp <- terra::rast("input/covariates/temp.tif")
+# # # Property size
 # PropSize <- terra::rast("input/covariates/prop_size.tif")
-# # Property value
+# # # Property value
 # PropVal <- terra::rast("input/covariates/prop_value.tif")
 
 # reclassify discrete covariates as appropriate
@@ -112,14 +87,16 @@ Temp <- terra::rast("input/covariates/temp.tif")
 # FEI TO WRITE CODE TO DO THIS - E.G., SEE LINES 35-37
 
 # create raster stack of continuous covariates
-StackCovsC <- terra::rast(list(Elev = Elev, Income = Income, PopDen = PopDen, Precip = Precip, Slope = Slope, SoilNit = SoilNit, Temp = Temp))
+StackCovsC <- terra::rast(list(PopDen16, PopGro16, SocioEcon16_PC, DistRoad, DistCity, prop_value, AgProf, TSoilPC, elev, slope, prec, temp , EcolCond))
+names(StackCovsC)[c(2, 10, 11, 15, 17, 18)] <- c("PopGro", "PropVal", "AgProf", "Elev", "Precip", "Temp")
 
 # save raster stack
 saveRDS(StackCovsC, file = "output/raster_stacks/cont_covs.rds")
 
 # create raster stack of discrete covariates
-StackCovsD <- terra::rast(list(Drought = Drought, Fire = Fire, ForCode = ForCode, ForTen = ForTen, ForTenType = ForTenType, LandUse = LandUse, Remote = Remote, SoilFert = SoilFert, SoilType = SoilType))
-
+StackCovsD <- terra::rast(list(PolPref , LandTen, NSW_forten18_ForTen, NSW_forten18_ForType, NatVegReg , PlanZone , drought , fire   ))
+names(StackCovsD)[c(7,8)] <- c("Drought", "Fire") 
+  
 # save raster stack
 saveRDS(StackCovsD, file = "output/raster_stacks/disc_covs.rds")
 
@@ -137,7 +114,7 @@ SUs <- list(CC = st_read("input/spatial_units/lots_kmrs.gdb", layer = "Central_C
 # add area to the spatial units attribute table
 for (i in names(SUs)) {
   SUs[[i]]$Shape_Area <- as.numeric(st_area(SUs[[i]]))
-  SUs[[i]]$Area = SUs[[i]]$Shape_Area / 10000
+  SUs[[i]]$Area = SUs[[i]]$Shape_Area / 1e4
 }
 
 # Check for Shape_Area < 0 
@@ -206,171 +183,46 @@ for (i in names(ZStats_CovsD)) {
 # save data
 saveRDS(ZStats_CovsD, file = "output/data/ZStats_CovsD.rds")
 
+# Combine SUs and Woody
+ZStats_Woody <- readRDS("output/data/ZStats_Woody.rds")
+ZStats_Woody_sf <- ZStats_Woody
+for (i in names(ZStats_Woody_sf)) {
+  ZStats_Woody_sf[[i]] <- ZStats_Woody_sf[[i]] %>% mutate(Shape = SUs[[i]]$Shape)
+}
+
 # combine SUs and Continuous covariates
+ZStats_CovsC <- readRDS("output/data/ZStats_CovsC.rds")
 ZStats_CovsC_sf <- ZStats_CovsC
 for (i in names(ZStats_CovsC_sf)) {
   ZStats_CovsC_sf[[i]] <- ZStats_CovsC_sf[[i]] %>% mutate(Shape = SUs[[i]]$Shape)
 }
+ZStats_CovsC_sf <- do.call(rbind, ZStats_CovsC_sf) 
 
-ZStats_CovsC_sf <- do.call(rbind, ZStats_CovsC_sf)
-st_write(ZStats_CovsC_sf, "input/ZStats_CovsC_sf.shp")
+# Check for NA values in continuous covariates 
 summary(ZStats_CovsC_sf)
-ZStats_CovsC_sf_NULL <- ZStats_CovsC_sf %>% filter(is.na(Elev))
 
-ggplot()+geom_sf(data = SUs$CC, fill = "white", color = "black")
+# Replace NA values with 9999 for continuous covariates for plotting in ArcGIS
+# ZStats_CovsC_sf <- ZStats_CovsC_sf %>% 
+#   mutate(Elev = replace_na(Elev, 9999),
+#          Income = replace_na(Income, 9999),
+#          PopDen = replace_na(PopDen, 999999),
+#          Precip = replace_na(Precip, 9999),
+#          Slope = replace_na(Slope, 999),
+#          SoilNit = replace_na(SoilNit, 999),
+#          Temp = replace_na(Temp, 9999))
 
-sf_data <- st_as_sf(ZStats_CovsC_sf_NULL, wkt = "shape")
-######################################
-# For checking normality of the data #
-######################################
-library(MASS)
-library(ggpubr)
-library(ggokabeito)
-theme_set(theme_pubr())
+# st_write(ZStats_CovsC_sf, "input/ZStats_CovsC_sf.shp", append = FALSE)
 
-# Read in all spatial units
-SUs <- readRDS("output/spatial_units/sus.rds")
 
-# Read in all continuous covariates
-ZStats_CovsC <- readRDS("output/data/ZStats_CovsC.rds")
-
-# Check for negative value in  continuous covariates 
-ZStats_CovsC_all <- do.call(rbind, ZStats_CovsC)
-map_dbl(ZStats_CovsC_all, min, na.rm = TRUE)
-
-# Include area in Continuous Covariates, remove units with area<0, +1+min(x) to all values
-for (i in names(ZStats_CovsC)) {
-  ZStats_CovsC[[i]] <- ZStats_CovsC[[i]] %>%
-    mutate(Area = SUs[[i]]$Area,
-           Elev = Elev+abs(floor(min(ZStats_CovsC_all$Elev, na.rm = TRUE))),
-           Income = Income+1,
-           PopDen = PopDen+1,
-           Slope = Slope+1) %>% 
-    filter(Area > 0)
+# combine SUs and Discrete covariates
+ZStats_CovsD <- readRDS("output/data/ZStats_CovsD.rds")
+ZStats_CovsD_sf <- ZStats_CovsD
+for (i in names(ZStats_CovsD_sf)) {
+  ZStats_CovsD_sf[[i]] <- ZStats_CovsD_sf[[i]] %>% mutate(Shape = SUs[[i]]$Shape)
 }
+ZStats_CovsD_sf <- do.call(rbind, ZStats_CovsD_sf) 
 
-# bind all continuous covariates for all spatial units
-ZStats_CovsC_all <- ZStats_CovsC
-for (i in names(ZStats_CovsC_all)) {
-  ZStats_CovsC_all[[i]] <- ZStats_CovsC_all[[i]] %>% mutate(KMR = i)
-}
-ZStats_CovsC_all <- do.call(rbind, ZStats_CovsC_all)
+# Check for NA values in Discrete covariates 
+summary(ZStats_CovsD_sf)
 
-# Create the histogram plot with facet_wrap
-hist_plot <- ggplot(ZStats_CovsC_long, aes(x = value)) +
-  geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
-  labs(title = "Histograms of Variables", x = "Value", y = "Frequency") +
-  facet_wrap(~name, scales = "free")
-
-# View the histogram plot
-hist_plot
-
-hist_Elev <- ggplot(ZStats_CovsC_all, aes(x = Elev, fill = KMR)) +
-  geom_histogram(binwidth = 10) +
-  scale_fill_okabe_ito()+
-  labs(x = "Elevation", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_Elev
-
-hist_Income <- ggplot(ZStats_CovsC_all, aes(x = Income, fill = KMR)) +
-  geom_histogram(binwidth = 100) +
-  scale_fill_okabe_ito()+
-  labs(x = "Income", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_Income
-
-hist_PopDen <- ggplot(ZStats_CovsC_all, aes(x = PopDen, fill = KMR)) +
-  geom_histogram(binwidth = 1000) +
-  scale_fill_okabe_ito()+
-  labs(x = "Population Density", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_PopDen
-
-hist_Precip <- ggplot(ZStats_CovsC_all, aes(x = Precip, fill = KMR)) +
-  geom_histogram(binwidth = 10) +
-  scale_fill_okabe_ito()+
-  labs(x = "Precipitation", y = "Frequency")+
-  theme(legend.position = "none")
-hist_Precip
-
-hist_Slope <- ggplot(ZStats_CovsC_all, aes(x = Slope, fill = KMR)) +
-  geom_histogram(binwidth = 1) +
-  scale_fill_okabe_ito()+
-  labs(x = "Slope", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_Slope
-
-hist_SoilNit <- ggplot(ZStats_CovsC_all, aes(x = SoilNit, fill = KMR)) +
-  geom_histogram(binwidth = 0.01) +
-  scale_fill_okabe_ito()+
-  labs(x = "Soil Nitrogen", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_SoilNit
-
-hist_Temp <- ggplot(ZStats_CovsC_all, aes(x = Temp, fill = KMR)) +
-  geom_histogram(binwidth = 1) +
-  scale_fill_okabe_ito()+
-  labs(x = "Temperature", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_Temp
-
-hist_Area <- ggplot(ZStats_CovsC_all, aes(x = Area, fill = KMR)) +
-  geom_histogram(binwidth = 10000) +
-  scale_fill_okabe_ito()+
-  labs(x = "Area", y = "Frequency")+
-  theme(legend.position = "none")
-# hist_Area
-
-hist_all <- ggarrange(hist_Elev, hist_Income, hist_PopDen, hist_Precip, hist_Slope, hist_SoilNit, hist_Temp, hist_Area, 
-                      ncol = 3, nrow = 3, common.legend = TRUE, legend="bottom")
-ggsave("hist_ZStats_CovsC.png", hist_all, width = 3000, height = 2000, units = "px", dpi = 300)
-
-
-
-ZStats_CovsC_LB <- data.frame(matrix(nrow = length(ZStats_CovsC), ncol = ncol(ZStats_CovsC[[1]])))
-colnames(ZStats_CovsC_LB) <- colnames(ZStats_CovsC_CC)
-rownames(ZStats_CovsC_LB) <- names(ZStats_CovsC)
-
-for(j in 1:length(ZStats_CovsC)){
-  for (i in 1:8) {
-    lm_mod <- lm(ZStats_CovsC[[j]][[i]] ~ 1, na.action = na.omit)
-    b <- boxcox(lm_mod, plotit = FALSE)
-    ZStats_CovsC_LB[j,i] <- b$x[which.max(b$y)]
-  }
-}
-
-ZStats_CovsC_all_LB <- data.frame(matrix(nrow = 1, ncol = ncol(ZStats_CovsC[[1]])))
-colnames(ZStats_CovsC_all_LB) <- colnames(ZStats_CovsC_CC)
-rownames(ZStats_CovsC_all_LB) <- "allKMR"
-
-for (i in 1:8) {
-    lm_mod <- lm(ZStats_CovsC_all[[i]] ~ 1, na.action = na.omit)
-    b <- boxcox(lm_mod, plotit = FALSE)
-    ZStats_CovsC_all_LB[1,i] <- b$x[which.max(b$y)]
-}
-
-ZStats_CovsC_LB <- rbind(ZStats_CovsC_LB, ZStats_CovsC_all_LB) %>% signif(1)
-
-ZStats_CovsC_LB_cl <- ZStats_CovsC_LB %>% signif(1) %>% 
-  mutate_all(function(x) {
-    ifelse(x <= -1.7, "1/x^2",
-           ifelse(x >= -1.3 & x <= -0.8, "1/x",
-                  ifelse(x >= -0.7 & x <= -0.3, "1/sqrt(x)",
-                         ifelse(x >= -0.2 & x <= 0.2, "log(x)",
-                                ifelse(x >= 0.3 & x <= 0.6, "sqrt(x)",
-                                       ifelse(x >= 0.7 & x <= 1.3, "x", 
-                                              ifelse(x >= 1.7, "x^2", x)))))))
-  })
-
-ZStats_CovsC_LB_tab <- ggtexttable(ZStats_CovsC_LB, theme = ttheme("blank")) %>%
-  tab_add_hline(at.row = 1:2, row.side = "top", linewidth = 2) %>% 
-  tab_add_hline(at.row = 10:11, row.side = "bottom", linewidth = 1) %>% 
-  tab_add_vline(at.column = c(1), column.side = "right", from.row = 2)
-ggsave("ZStats_CovsC_LB_tab.png", ZStats_CovsC_LB_tab, width = 3000, height = 2000, units = "px", dpi = 300, bg = 'white')
-
-ZStats_CovsC_LB_cl_tab <- ggtexttable(ZStats_CovsC_LB_cl, theme = ttheme("blank")) %>%
-  tab_add_hline(at.row = 1:2, row.side = "top", linewidth = 2) %>% 
-  tab_add_hline(at.row = 10:11, row.side = "bottom", linewidth = 1) %>% 
-  tab_add_vline(at.column = c(1), column.side = "right", from.row = 2)
-ggsave("ZStats_CovsC_LB_cl_tab.png", ZStats_CovsC_LB_cl_tab, width = 3000, height = 2000, units = "px", dpi = 300, bg = 'white')
-
+# st_write(ZStats_CovsD_sf, "input/ZStats_CovsD_sf.shp", append = FALSE)
